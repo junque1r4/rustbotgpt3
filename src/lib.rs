a6a34280db6c4883f453363646468ebae905e556
João <dev.junqueira@gmail.com>
2023-03-10 17:31:08 -0300

    Added conection to new chatgpt-3 model

--- src/lib.rs
+++ src/lib.rs
@@ -2,37 +2,50 @@ use serde_derive::{Deserialize, Serialize};
 use isahc::{prelude::*, Body, Request};
 
 
-#[derive(Deserialize, Debug)]
-struct OAIChoises {
-	text: String,
+#[derive(Serialize, Deserialize, Debug)]
+struct OAIChoices {
+    role: String,
+	content: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ChoicesTemplate {
+    index: i32,
+    message: OAIChoices,
+    finish_reason: String
 }
 
 #[derive(Deserialize, Debug)]
 struct OAIResponse {
-	choices: Vec<OAIChoises>,
+	choices: Vec<ChoicesTemplate>,
 }
 
 #[derive(Serialize, Debug)]
 struct OAIRequest {
-	prompt: String,
+	messages: Vec<MessageModel>,
 	model: String,
 	max_tokens: u32,
-	temperature: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MessageModel {
+    role: String,
+    content: String,
 }
 
 pub fn chatbot(user_text: String, openai_token: String) -> Option<String> {
-    let openai_url = "https://api.openai.com/v1/completions";
-    let preamble = "Devo responder tudo de forma irônica e sarcástica.";
+    let openai_url = "https://api.openai.com/v1/chat/completions";
 
     let auth_header_val = format!("Bearer {}", openai_token);
 
     let openai_request = OAIRequest {
-        prompt: format!("{}{}", preamble, user_text),
-        model: String::from("text-davinci-003"),
-        max_tokens: 100,
-        temperature: 0,
+        messages: vec![ MessageModel {
+            role: "user".to_string(),
+            content: user_text,
+        }],
+        model: String::from("gpt-3.5-turbo-0301"),
+        max_tokens: 350,
     };
-
 
     let requisition_body = Body::from(serde_json::to_vec(&openai_request).unwrap());
     let response = Request::post(openai_url)
@@ -44,12 +57,17 @@ pub fn chatbot(user_text: String, openai_token: String) -> Option<String> {
     // response as json
     match response {
         Some(mut response_body) => {
-            let response_json: OAIResponse = serde_json::from_str(&response_body.text().unwrap()).unwrap();
-            Some(response_json.choices[0].text.trim().to_string())
+            let response_json= serde_json::from_str(&response_body.text().unwrap());
+            let unraped: OAIResponse  = match response_json {
+                Ok(a) => {
+                    a
+                },
+                Err(_) => {
+                    return None;
+                }
+            };
+            Some(unraped.choices[0].message.content.trim().to_string())
         },
         None => None,
     }
-    
 }
-
-
